5ac7ed242dc6d3d7489ca3861ddaf6c683d5f9a9
Hanna Pereverzieva <h.pereverzieva@lgbs.pl>
2015-11-15 13:55:48 +0100

    uporzadkowanie w projekcie WcfService

---
+++ SailingPerformance/WcfService/BaseResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService
+{
+    [DataContract]
+    public class BaseResponse
+    {
+        [DataMember]
+        public bool IsSuccess { get; set; }
+        [DataMember]
+        public string ErrorMessage { get; set; }
+    }
+}
--- SailingPerformance/WcfService/Services/UserService/UserService.svc.cs
+++ SailingPerformance/WcfService/Services/UserService/UserService.svc.cs
@@ -1,4 +1,4 @@
-﻿namespace WcfService
+﻿namespace WcfService.Services.UserService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
